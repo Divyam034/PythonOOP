fc8d9d116a164c50e87467957838a5b82b7e8622
Divyam <divyam2116@gmail.com>
2022-06-25 21:02:43 +0530

    added some files

--- PythonOOP.py
+++ PythonOOP.py
@@ -1,4 +1,18 @@
-class Computer:
-    pass
+from multiprocessing import cpu_count
 
-comp1 = Computer()
+
+"""
+class Computer:
+    def __init__(self, cpu, ram):
+        self.cpu = cpu
+        self.ram = ram
+    def config(self):
+        print("The configuration is:", self.cpu, self.ram)
+
+comp1 = Computer("i5", 8)
+comp2 = Computer("ryzen 3", 16)
+
+comp1.config()
+comp2.config()
+"""
+
